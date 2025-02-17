4a48e42bc7a6fb9c8a86df0b2b6749731cf30ee0
Imran-az <76496687+Imran-az@users.noreply.github.com>
2025-02-16 19:53:25 -0500

    fix: small variable bug

--- countdown-customizer/src/app/components/CountdownTimer.js
+++ countdown-customizer/src/app/components/CountdownTimer.js
@@ -15,7 +15,7 @@ const CountdownTimer = ({ targetDate, color, textSize, font }) => {
     }
     const [timeLeft, setTimeLeft] = useState({ days: 0,
                                                 hours: 0,
-                                                minute: 0,
+                                                minutes: 0,
                                                 seconds: 0,});
 
     useEffect(() => {
